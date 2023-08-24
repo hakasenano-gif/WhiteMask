c47dc65b9148c976fbcd751206d3c7b9013adf97
sutsuk <107033259+sutsuk@users.noreply.github.com>
2023-08-24 11:04:41 +0900

    Update Build

---
+++ Build/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "WhiteMask-WhiteMask-1.0";
+const contentToCache = [
+    "Build/WhiteMask_WebGL.loader.js",
+    "Build/WhiteMask_WebGL.framework.js.unityweb",
+    "Build/WhiteMask_WebGL.data.unityweb",
+    "Build/WhiteMask_WebGL.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
